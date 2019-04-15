3253ef60ca7bdcc80749de1cb11f1f5b42241707
jbot <>
2019-04-14 18:54:27 -0700

    Logo working with the Sharp transformer, currently stretched

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { StaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
 
+/*
+* This component is built using `gatsby-image` to automatically serve optimized
+* images with lazy loading and reduced file sizes. The image is loaded using a
+* `StaticQuery`, which allows us to load the image from directly within this
+* component, rather than having to pass the image data down from pages.
+*
+* For more information, see the docs:
+* - `gatsby-image`: https://gatsby.dev/gatsby-image
+* - `StaticQuery`: https://gatsby.dev/staticquery
+*/
 
-const Logo = () => (
-  <div>
-    <title>LekoArts</title>
-    <img src="/logos/pfa-logo.png" width="128" height="128"/>
-  </div>
-)
-
-export default Logo
+const Image = () => (
+	<StaticQuery
+		query={graphql`
+			query {
+				placeholderImage: file(relativePath: { eq: "pfa-logo.png" }) {
+					childImageSharp {
+						fixed {
+							...GatsbyImageSharpFixed
+						}
+					}
+				}
+			}
+		`}
+		render={(data) => <Img fixed={data.placeholderImage.childImageSharp.fixed} />}
+	/>
+);
+export default Image;
